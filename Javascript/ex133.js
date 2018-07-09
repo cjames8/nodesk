502ef98fee42287e52c506f0dfc8bd83c144b480
Carlene <carlenejames781@gmail.com>
2018-07-08 18:25:12 -0600

    Added 133 updates

--- Javascript/ex133.js
+++ Javascript/ex133.js
@@ -1,6 +1,11 @@
 const mutants = ['Professor X', 'Cyclops', 'Iceman', 'Angel', 'Magneto', 'Beast', 'Phoenix', 'Logan', 'Gambit']
 
-for(counter = 0; counter= muntants.length; counter++){
-    
+for(counter = 0; counter < mutants.length; counter++){
+    console.log (mutants);
 }
-
+if (muntants == 'Magneto') {
+    return;
+}
+else {
+    console.log(mutants);
+}
